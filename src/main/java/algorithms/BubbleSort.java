6cb2619d4a2f3313df1152410732f7baf1e7c90d
Krystian Woźniak <krys.blu@gmail.com>
2019-06-10 20:22:52 +0200

    Implement: BubbleSort algorithm

---
+++ src/main/java/algorithms/BubbleSort.java
@@ -0,0 +1,35 @@
+package algorithms;
+
+public class BubbleSort {
+
+
+    static void bubbleSort(int[] array) {
+        boolean sorted = false;
+        int temp;
+
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+    }
+
+    static void bubbleSortKobietyDoKodu(int[] collection) {
+        for (int i = 0; i < collection.length; i++) {
+            for (int j = 1; j < collection.length; j++) {
+                if (collection[j] < collection[j - 1]) {
+                    int temp = collection[j - 1];
+                    collection[j - 1] = collection[i];
+                    collection[i] = temp;
+                }
+            }
+        }
+    }
+
+}
