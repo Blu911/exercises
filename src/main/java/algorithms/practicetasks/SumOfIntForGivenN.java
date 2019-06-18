d353aa5be59b71e51347b12d932dd6d0baba1ba5
Krystian Woźniak <krys.blu@gmail.com>
2019-06-18 20:48:21 +0200

    Implement: sumOfIntegersForGivenN

---
+++ src/main/java/algorithms/practicetasks/SumOfIntForGivenN.java
@@ -0,0 +1,33 @@
+package algorithms.practicetasks;
+/**
+Problem: For given n calculate the sum of integers from 1 to n
+ **/
+public class SumOfIntForGivenN {
+    public static void main(String[] args) {
+        System.out.println(solutionO1(3));
+    }
+// fast solution n < 10 000, n < 1 000 000, n < 10^10
+    static int solutionO1(int n) {
+        return n * (n+1)/2;
+
+    }
+// slow solution n < 10 000, n < 1 000 000
+    static int soultionOn(int n) {
+        int result = 0;
+        for (int i = 1; i <=n; i++) {
+            result += i;
+        }
+        return result;
+    }
+// brute solution n < 10 000
+    static int solutionOn2(int n) {
+        int result = 0;
+
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= i; j++) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
